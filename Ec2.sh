f3357b48e4b1f1c783ebee1c76390bf3babd1fa8
Mohammadfazal-sayed <fazal92.sayed@gmail.com>
2020-08-12 17:57:00 +0530

    New

--- Ec2.sh
+++ Ec2.sh
@@ -4,5 +4,5 @@ aws ec2 run-instances \
     --subnet-id subnet-7f879441 \
     --security-group-ids sg-07142950944cc283c \
     --associate-public-ip-address \
-    --key-name sbk \
+    --key-name sbp \
     --region us-east-1
